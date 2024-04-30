f9a6ff499c77158e5853d5f57cd983c71e8b30dc
harshleet <harshguptalnm@gmail.com>
2024-04-30 21:44:01 +0530

    Update DisplayCampaigns.jsx

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import FundCard from './FundCard';
-import { loader } from '../assets';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import FundCard from "./FundCard";
+import { loader } from "../assets";
 
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
@@ -12,13 +12,19 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   };
 
   return (
-    <div className='border border-dashed p-10'>
+    <div className="border border-dashed p-10">
       <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
         {title} ({campaigns.length})
       </h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
-        {isLoading && <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />}
+        {isLoading && (
+          <img
+            src={loader}
+            alt="loader"
+            className="w-[100px] h-[100px] object-contain"
+          />
+        )}
 
         {!isLoading && campaigns.length === 0 && (
           <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
